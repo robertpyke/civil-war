52defa42fd1b4637d429f1178146216ea14e3c66
robertpyke <robert.j.pyke@gmail.com>
2012-06-18 13:11:10 +1000

    Init'd with rails 3.2.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoScorchServer::Application.config.secret_token = '232c2c038d09668b7fe0794d1a59b015ac00e5d4b85c0b661b8564f01bff152031dad0f8441d241b4e53cea18d1e1cc063a7cb166390d9d27638d47fec7f610c'
