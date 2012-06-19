040dc122020273c69b1d5408825078cf1b71ea59
Robert Pyke <robert.j.pyke@gmail.com>
2012-06-19 14:28:12 +1000

    Added facebook device app details.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!, :only => [:index]
+
   # GET /users
   # GET /users.json
   def index
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -207,7 +207,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   require "omniauth-facebook"
-  config.omniauth :facebook, "APP_ID", "APP_SECRET"
+  config.omniauth :facebook, "245482665561715", "a00a3dea095d502b3c536bb797134b7c"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
