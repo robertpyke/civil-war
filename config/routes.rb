a39b0ab95d267cf5da5900527530071ca6ef2227
Robert Pyke <robert.j.pyke@gmail.com>
2012-06-20 15:35:50 +1000

    Changed code to ONLY support omnioauth.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 GeoScorchServer::Application.routes.draw do
+#  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+#    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
+#  end
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  end
+
+  devise_scope :user do
     get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
   end
 
