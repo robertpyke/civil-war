61ebe8a4f4a316908d56575280ace81d34e8b558
Robert Pyke <robert.j.pyke@gmail.com>
2012-06-20 16:29:58 +1000

    working on omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ GeoScorchServer::Application.routes.draw do
 #    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
 #  end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
-    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
-  end
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_scope :user do
     get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
