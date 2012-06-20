d3d65e6ba6d3a49d1176ca047e9fb97259daf364
Robert Pyke <robert.j.pyke@gmail.com>
2012-06-20 16:19:07 +1000

    working on devise support.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ GeoScorchServer::Application.routes.draw do
 #  end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
-    get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
-    get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
   end
 
   devise_scope :user do
